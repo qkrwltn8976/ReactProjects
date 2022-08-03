d35b2ac7b545da7f176f11e93e1e81774b198d56
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2022-08-03 22:01:31 +0900

    docs: typescript day 10

--- typescript-challenge/4-5.ts
+++ typescript-challenge/4-5.ts
@@ -9,15 +9,15 @@ class LocalStorage<T> {
   // 제네릭을 클래스로 보내고 클래스는 제네릭을 인터페이스로 보내면 인터페이스는 제네릭을 사용
   private storage: SStorage<T> = {};
 
-  set(key: string, value: T) {
+  setItem(key: string, value: T) {
     this.storage[key] = value;
   }
 
-  remove(key: string) {
+  clearItem(key: string) {
     delete this.storage[key];
   }
 
-  get(key: string): T {
+  getItem(key: string): T {
     return this.storage[key];
   }
 
@@ -28,10 +28,10 @@ class LocalStorage<T> {
 
 const stringsStorage = new LocalStorage<string>();
 
-stringsStorage.get("key");
-stringsStorage.set("hello", "how are you");
+stringsStorage.getItem("key");
+stringsStorage.setItem("hello", "how are you");
 
 const booleansStorage = new LocalStorage<boolean>();
 
-booleansStorage.get("xxx");
-booleansStorage.set("hello", true);
+booleansStorage.getItem("xxx");
+booleansStorage.setItem("hello", true);
