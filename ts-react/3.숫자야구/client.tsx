d71e0429e93a6166e7d6986cc799709a1c9c3f6a
qkrwltn <qkrwltn8976@naver.com>
2022-08-08 14:52:09 +0900

    doc: 웹 게임을 만들며 배우는 React에 Typescript 적용하기: 3. 숫자야구

---
+++ ts-react/3.숫자야구/client.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import NumberBaseball from "./NumberBaseball";
+ReactDOM.render(<NumberBaseball />, document.querySelector("#root"));
