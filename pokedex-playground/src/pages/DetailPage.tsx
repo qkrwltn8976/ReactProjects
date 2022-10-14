0bf911d7ffa3e6915897e64814882cd144eda4c7
qkrwltn <qkrwltn8976@naver.com>
2022-10-14 16:59:07 +0900

    feat: react-query를 이용해 포켓몬 도감 만들기 - Detail 스타일링

--- pokedex-playground/src/pages/DetailPage.tsx
+++ pokedex-playground/src/pages/DetailPage.tsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import Tabs from "../components/Tabs";
 
 type Params = {
-    id: string;
-}
-const DetailPage: React.FC = () => {
-    const { id } = useParams<Params>()
-    return (
-        <div>DetailPage id: {id}</div>
-    )
-}
+  id: string;
+};
 
-export default DetailPage
+type Tab = "about" | "stats" | "evolution";
+
+const DetailPage: React.FC = () => {
+  const { id } = useParams<Params>();
+  const [selectedTab, setSelectedTab] = useState<Tab>("about");
+
+  const handleClick = (tab: Tab) => {
+    setSelectedTab(tab);
+  };
+  return (
+    <div>
+      <Tabs
+        tab={selectedTab}
+        onClick={handleClick}
+        color={{ name: "red", url: "" }}
+      />
+    </div>
+  );
+};
+
+export default DetailPage;
